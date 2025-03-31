509fc4dda268e403cd84b24cc5deed333f81dd46
Muhammad Shahid <me911khan@gmail.com>
2025-03-31 15:20:50 +0200

    checkbox ngModule

--- src/app/components/my-first-component/my-first-component.component.ts
+++ src/app/components/my-first-component/my-first-component.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-my-first-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './my-first-component.component.html',
   styleUrl: './my-first-component.component.css'
 })
 export class MyFirstComponentComponent {
 
   title: string = 'My First Component';
+  checkBoxValue: boolean = false;
+  // checkBoxValue1!: boolean;
+  checkBoxValue2: boolean = false;
 
   user: any = {
     name: 'Shahid',
@@ -25,4 +29,29 @@ export class MyFirstComponentComponent {
     }, 2000)
   }
 
+  handleClick() {
+    alert('See ! you just click the magic button');
+  }
+
+  handleSubmit(e: Event): void {
+    e.preventDefault();
+    console.log(e);
+    alert('Form submitted!');
+
+    const target = e.target as HTMLFormElement;
+    const formDate = new FormData(target);
+
+    const name: string = formDate.get('name')!.toString();
+    const age: number = Number(formDate.get('age'));
+
+    alert(`Name: ${name}, Age: ${age}`);
+
+    
+  }
+
+  handleCheckBox(e: Event): void {
+
+    const target = e.target as HTMLInputElement;
+    this.checkBoxValue = target.checked;
+  }
 }
